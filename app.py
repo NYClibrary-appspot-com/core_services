c207d25c3fca5d137fa65fe6277c09e4e175079f
tusharcu12@gmail.com <tusharcu12@gmail.com>
2019-11-12 12:45:20 -0500

    change to app

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-# root
-@app.route("/")
+# Root
+@app.route("/", methods=['GET'])
 def helloWorld():
     return "welcome to NYC Virtul library"
 
 
 if __name__ == "__main__":
     # Windows HOST
-    app.run(port=5000, debug=True, host='127.0.0.1')
+    # app.run(port=5000, debug=True, host='127.0.0.1')
 
     # # Linux HOST
-    # app.run(port=5000, debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, threaded=True)

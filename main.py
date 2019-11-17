acf53a152bbd8a63c347e50e55633157cbaa9c80
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-11-17 16:08:22 -0500

    exception handler added

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 from flask import Flask, request
 from google.cloud import storage
 
-
 app = Flask(__name__)
 CORS(app)
 rawPath = "serviceAccount.json"
@@ -35,22 +34,36 @@ def list_of_books():
             list.append(book)
         return json.dumps(list)
     except Exception as e:
-        return json.dumps({"error": "somethings went wrong"})
+        json.dumps({"error": "exception found"})
 
 
 @app.route("/search", methods=['GET'])
 def search():
     # http://127.0.0.1:5000/search?book_name=requirements.txt
-
+    # http://127.0.0.1:5000/search?book_name=t # prefix search
     book_name = request.args.get('book_name')
-    status = search_a_book(book_name)
-    return status
+    try:
+        status = search_a_book(book_name)
+        return status
+    except Exception as e:
+        return json.dumps({"error": "exception found"})
 
 
 # view without downloading the file
-def search_a_book(book_name):
+def search_a_book(book_name):  # search by actual file name
     blob = bucket.get_blob(book_name)
-    if blob is None:
+
+    if blob is None:  # search by prefix
+        pre_list = bucket.list_blobs(prefix=book_name)
+
+        if pre_list is not None:
+            list = []
+            for value in pre_list:
+                book = value.name
+                list.append(book)
+            if len(list) > 0:
+                return json.dumps(list)
+
         return json.dumps({'error': "file not found"})
     else:
         return json.dumps({'success': True, 'book_name': '{}'.format(blob.name)})
@@ -64,16 +77,18 @@ def add_books():
     param: book_name
     :return: 'success' or 'error'
     """
-    file = request.files['fi']
-    blob = bucket.blob(file.filename)
-    blob.upload_from_file(file)
+    try:
+        file = request.files['fi']
+        blob = bucket.blob(file.filename)
+        blob.upload_from_file(file)
 
-    status = search_a_book(file.filename)
-    if 'success' in status:
-        return json.dumps({"success": "File uploaded successfully!"})
-    else:
-        return json.dumps({"error": "File was not uploaded"})
-
+        status = search_a_book(file.filename)
+        if 'success' in status:
+            return json.dumps({"success": "File uploaded successfully!"})
+        else:
+            return json.dumps({"error": "File was not uploaded"})
+    except Exception as e:
+        json.dumps({"error": "exception found"})
 
 
 @app.route("/download_book", methods=['GET'])
@@ -82,16 +97,18 @@ def download_a_book():
     Downloads a blob from the bucket.
     http://127.0.0.1:5000/download_book?book_name=FALL2019.PNG
     """
-    book_name = request.args.get('book_name')
-    record = search_a_book(book_name)
-    if 'success'in record:
-        blob = bucket.blob(book_name)
-        downloads = os.path.join(os.path.join(os.path.expanduser('~')), 'Downloads')
-        blob.download_to_filename(downloads+"\\"+book_name)
-        return json.dumps({'book': book_name, "status": "please check the download folder"})
-    else:
-        return json.dumps(record)
-
+    try:
+        book_name = request.args.get('book_name')
+        record = search_a_book(book_name)
+        if 'success' in record:
+            blob = bucket.blob(book_name)
+            downloads = os.path.join(os.path.join(os.path.expanduser('~')), 'Downloads')
+            blob.download_to_filename(downloads + "\\" + book_name)
+            return json.dumps({'book': book_name, "status": "please check the download folder"})
+        else:
+            return json.dumps(record)
+    except Exception as e:
+        json.dumps({"error": "exception found"})
 
 
 if __name__ == "__main__":
